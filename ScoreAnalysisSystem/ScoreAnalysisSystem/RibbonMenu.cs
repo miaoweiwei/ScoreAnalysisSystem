e41415c0c2e58501fa40ba062e8de63bcadd2b1a
苗子 <1353263604@qq.com>
2019-01-10 14:15:37 +0800

    增加日志记录模块

--- ScoreAnalysisSystem/ScoreAnalysisSystem/RibbonMenu.cs
+++ ScoreAnalysisSystem/ScoreAnalysisSystem/RibbonMenu.cs
@@ -10,20 +10,42 @@ namespace ScoreAnalysisSystem
     [ComVisible(true)]
     public class RibbonMenu : ExcelRibbon
     {
+        private static readonly log4net.ILog Log = log4net.LogManager.GetLogger("RibbonMenu");
+        private IRibbonUI _ribbonUi;
+
         public void RibbonMenu_Load(IRibbonUI ribbonUi)
         {
-
+            Log.Debug("初始化成功");
+            _ribbonUi = ribbonUi;
         }
 
-        public void btnLogin_Click(IRibbonControl control)
+        public void btnAnalysis_Click(IRibbonControl control)
         {
-
+            //_ribbonUi.InvalidateControl("btnAnalysis");
+            _ribbonUi.Invalidate(); //刷新显示
         }
 
-        private string _loginLable = "登录";
-        public string GetLoginLabel(IRibbonControl control)
+        private string _analysisLabel = "分析成绩";
+
+        public string GetAnalysisLabel(IRibbonControl control)
         {
-            return _loginLable;
+            return _analysisLabel;
+        }
+
+        /// <summary>
+        /// 刷新全部控件
+        /// </summary>
+        private void RefreshControl()
+        {
+            _ribbonUi.Invalidate();
+        }
+
+        /// <summary>
+        /// 刷新指定Id的控件
+        /// </summary>
+        private void RefreshControl(string controlId)
+        {
+            _ribbonUi.InvalidateControl(controlId);
         }
     }
 }
