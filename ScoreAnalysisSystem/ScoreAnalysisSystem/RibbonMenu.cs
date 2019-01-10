defc4e334022058609ab75bc83be8050a80047ed
苗子 <1353263604@qq.com>
2019-01-10 12:57:17 +0800

    工程创建

---
+++ ScoreAnalysisSystem/ScoreAnalysisSystem/RibbonMenu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using ExcelDna.Integration.CustomUI;
+
+namespace ScoreAnalysisSystem
+{
+    [ComVisible(true)]
+    public class RibbonMenu : ExcelRibbon
+    {
+        public void RibbonMenu_Load(IRibbonUI ribbonUi)
+        {
+
+        }
+
+        public void btnLogin_Click(IRibbonControl control)
+        {
+
+        }
+
+        private string _loginLable = "登录";
+        public string GetLoginLabel(IRibbonControl control)
+        {
+            return _loginLable;
+        }
+    }
+}
